041779d0828cd59eb587c00eb21d723c7ce49d5d
Jordan Lira de Araújo junior <jordanjr.92@gmail.com>
2014-02-22 12:57:26 -0200

    Pegando primeiro elemento
    adicionado a função get_first() pra pegar o ultimo elemento e verificar se ele é igual ao da lista passada

--- ReadAndPress.py
+++ ReadAndPress.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import uinput
 
+global elemAnterior = None
 browser = webdriver.Firefox()
 browser.get('http://www.twitch.tv/araujojordan')
 
@@ -22,36 +24,47 @@ teclas = (
 	)
 device = uinput.Device(teclas)
 
+def get_first(iterable):
+	if iterable:
+		item = iterable[-1]
+			if(item == elemAnterior)
+				return None
+			elemAnterior = item
+			return item
+	return None
+
 while True:
 	try:
 		elements = browser.find_elements_by_class_name("chat_line")
-		for elem in elements:
-
-			comando = elem.get_attribute("innerHTML")
-			if comando == 'up' or comando == 'UP' or comando == 'Up':
-				device.emit_click(uinput.KEY_UP)
-			elif comando == 'down' or comando == 'DOWN' or comando == 'Down':
-				device.emit_click(uinput.KEY_DOWN)
-			elif comando == 'right' or comando == 'RIGHT' or comando == 'Right':
-				device.emit_click(uinput.KEY_RIGHT)
-			elif comando == 'left' or comando == 'LEFT' or comando == 'Left':
-				device.emit_click(uinput.KEY_LEFT)
-			elif comando == 'b' or comando == 'B':
-				device.emit_click(uinput.KEY_B)
-			elif comando == 'start' or comando == 'Start' or comando == 'START':
-				device.emit_click(uinput.KEY_P)
-			elif comando == 'select' or comando == 'Select' or comando == 'SELECT':
-				device.emit_click(uinput.KEY_S)
-			elif comando == 'a' or comando == 'A':
-				device.emit_click(uinput.KEY_A)
-			elif comando == 'X' or comando == 'x':
-				device.emit_click(uinput.KEY_X)
-			elif comando == 'Y' or comando == 'y':
-				device.emit_click(uinput.KEY_Y)
-			elif comando == 'L' or comando == 'l':
-				device.emit_click(uinput.KEY_L)
-			elif comando == 'R' or comando == 'r':
-				device.emit_click(uinput.KEY_R)
+		elem = get_first(elements)
+		if(elem == None)
+			continue
+		#for elem in elements:
+		comando = elem.get_attribute("innerHTML")
+		if comando == 'up' or comando == 'UP' or comando == 'Up':
+			device.emit_click(uinput.KEY_UP)
+		elif comando == 'down' or comando == 'DOWN' or comando == 'Down':
+			device.emit_click(uinput.KEY_DOWN)
+		elif comando == 'right' or comando == 'RIGHT' or comando == 'Right':
+			device.emit_click(uinput.KEY_RIGHT)
+		elif comando == 'left' or comando == 'LEFT' or comando == 'Left':
+			device.emit_click(uinput.KEY_LEFT)
+		elif comando == 'b' or comando == 'B':
+			device.emit_click(uinput.KEY_B)
+		elif comando == 'start' or comando == 'Start' or comando == 'START':
+			device.emit_click(uinput.KEY_P)
+		elif comando == 'select' or comando == 'Select' or comando == 'SELECT':
+			device.emit_click(uinput.KEY_S)
+		elif comando == 'a' or comando == 'A':
+			device.emit_click(uinput.KEY_A)
+		elif comando == 'X' or comando == 'x':
+			device.emit_click(uinput.KEY_X)
+		elif comando == 'Y' or comando == 'y':
+			device.emit_click(uinput.KEY_Y)
+		elif comando == 'L' or comando == 'l':
+			device.emit_click(uinput.KEY_L)
+		elif comando == 'R' or comando == 'r':
+			device.emit_click(uinput.KEY_R)
 
 	except Exception: #Erro acontece quando um elemento eh deletado ou reaproveitado pelo codigo
 		continue #melhor opcao econtrada eh ignorar para que ele passe para o proximo
