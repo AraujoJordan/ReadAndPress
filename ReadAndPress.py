79ae49d2bb02f7b5dc84a95cdfd739cbdcfff425
Jordan Lira <jordanjr.92@gmail.com>
2014-02-25 12:33:41 -0300

    versao com threads

---
+++ ReadAndPress.py
@@ -0,0 +1,42 @@
+from pykeyboard import PyKeyboard #need Xlib
+from threading import Thread
+import time
+
+k = PyKeyboard()
+
+#	Hold the key for 0.5 seconds
+def hold(character):
+    k.press_key(character)
+    time.sleep(0.5)
+    k.release_key(character)	
+
+#	Read and Press the comments list (SNES CONTROL)
+def readAndPress(chat_list):
+	for chat_line in chat_list:
+		if chat_line == 'up' or chat_line == 'UP' or chat_line == 'Up':
+			hold(k.up_key)
+		elif chat_line == 'down' or chat_line == 'DOWN' or chat_line == 'Down':
+			hold(k.down_key)
+		elif chat_line == 'right' or chat_line == 'RIGHT' or chat_line == 'Right':
+			hold(k.right_key)
+		elif chat_line == 'left' or chat_line == 'LEFT' or chat_line == 'Left':
+			hold(k.left_key)
+		elif chat_line == 'start' or chat_line == 'Start' or chat_line == 'START':
+			k.type_string('p')
+		elif chat_line == 'select' or chat_line == 'Select' or chat_line == 'SELECT':
+			k.type_string('s')
+		elif chat_line == 'b' or chat_line == 'B':
+			k.type_string('b')
+		elif chat_line == 'a' or chat_line == 'A':
+			k.type_string('a')
+		elif chat_line == 'X' or chat_line == 'x':
+			k.type_string('x')
+		elif chat_line == 'Y' or chat_line == 'y':
+			k.type_string('y')
+		elif chat_line == 'L' or chat_line == 'l':
+			k.type_string('l')
+		elif chat_line == 'R' or chat_line == 'r':
+			k.type_string('r')
+		else:
+			return
+		print chat_line
--- Start.py
+++ Start.py
@@ -1,25 +1,31 @@
-from Press import *
-from Read import *
+from ReadAndPress import *
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+URL = 'http://www.twitch.tv/twitchplayspokemon' #you can use www.justin.tv too
+
+#	This method check if the chat list is >= 1 element
+#	then, read all coments in a new thread.
+def readAndClear(chat_list,browser):
+	if(len(chat_list)>=1:
+		readList = list(chat_list)
+		thread = Thread(target = readAndPress, args = (chat_list))
+		thread.start()
+		browser.execute_script("CurrentChat.clear_chat_lines()") #clear the browser chat
+
+#	Call the browser Firefox and read the coments in the chat of Twitch/Justin
 def connect(url):
-	global listLen
-	global lastElemID
-
 	browser = webdriver.Firefox()
-	browser.get('http://www.twitch.tv/'+url)
+	browser.get(url)
 
-	while True:
-		
+	#loop until the CTRL+C (to stop the program)
+	while True:		
 		try:
-			item_list = browser.find_elements_by_class_name("chat_line")
+			chat_list = browser.find_elements_by_class_name("chat_line")
 		except Exception:
-			pass
+			readAndClear(chat_list,browser)
+			continue
+		readAndClear(chat_list,browser)
 
-		press(read(item_list))
-		if(len(item_list)>=150):
-			browser.execute_script("CurrentChat.clear_chat_lines()")
-			lastElemID = 'nothing'
-			listLen = 'nothing'
-
-
-connect('araujojordan')
+#begin
+connect(URL)
